11c8100eaf5d7342f38806cdff52e5422e477236
Phil Howard <phil@gadgetoid.com>
2019-03-13 12:26:40 +0000

    Added intros to examples

--- examples/randomblinky.py
+++ examples/randomblinky.py
@@ -11,6 +11,14 @@ except ImportError:
 
 from rgbmatrix5x5 import RGBMatrix5x5
 
+print("""
+RGBMatrix5x5 - Random Blinky!
+
+Displays random warm colours across the matrix.
+
+Press Ctrl+C to exit!
+""")
+
 rgbmatrix5x5 = RGBMatrix5x5()
 
 rgbmatrix5x5.set_clear_on_exit()
@@ -26,10 +34,7 @@ while True:
             h = 0.1 * rand_mat[x, y]
             s = 0.8
             v = rand_mat[x, y]
-            rgb = colorsys.hsv_to_rgb(h, s, v)
-            r = int(rgb[0] * 255.0)
-            g = int(rgb[1] * 255.0)
-            b = int(rgb[2] * 255.0)
+            r, g, b = [int(c * 255) for c in colorsys.hsv_to_rgb(h, s, v)]
             rgbmatrix5x5.set_pixel(x, y, r, g, b)
     rgbmatrix5x5.show()
     time.sleep(0.01)
