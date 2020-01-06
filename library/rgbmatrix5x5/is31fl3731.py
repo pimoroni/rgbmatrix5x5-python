f17bc54cbe6ab8da27f89bbda167e645585a9f1e
Septolum <Septolum@gmail.com>
2020-01-06 13:33:57 +0000

    Fix whitespace error

--- library/rgbmatrix5x5/is31fl3731.py
+++ library/rgbmatrix5x5/is31fl3731.py
@@ -90,7 +90,7 @@ class Matrix:
                     raise ImportError('This library requires python-smbus\nInstall with: sudo apt-get install python-smbus')
                 elif version_info[0] == 3:
                     raise ImportError('This library requires python3-smbus\nInstall with: sudo apt-get install python3-smbus')
-          
+
             try:
                 self.i2c = smbus.SMBus(1)
             except IOError as e:
